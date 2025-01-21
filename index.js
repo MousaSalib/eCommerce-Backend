d0400a32cb381aada2b00c0e8823f78a69bc673d
Mousa Khairy Halim Salib <moussa.kheiry@gmail.com>
2025-01-21 14:43:23 +0200

    I defined the port and make some changes on the package.json file

--- index.js
+++ index.js
@@ -6,11 +6,19 @@ const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
 
+// Middleware
 server.use(middlewares);
 server.use(cors());
 server.db = router.db;
 
+// Authentication and routes
 server.use(auth);
 server.use(router);
 
-module.exports = server;
+// Define the port
+const PORT = process.env.PORT || 5005;
+
+// Start the server
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
